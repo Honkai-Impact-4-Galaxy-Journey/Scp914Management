acd4afd01eb1a56bf29dac92184ef9a222370891
Griseo-bh3rd <atmlz32@163.com>
2025-05-28 16:06:53 +0800

    feat: add item recipe override

--- Scp914Management/PluginMain.cs
+++ Scp914Management/PluginMain.cs
@@ -3,10 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LabApi.Events.Arguments.Scp914Events;
+using LabApi.Events.Handlers;
+using LabApi.Features.Console;
+using LabApi.Loader.Features.Plugins;
 
 namespace Scp914Management
 {
-    public class PluginMain
+    public class Config
     {
+        public List<BaseItemRecipe> ItemRecipes { get; set; } = new List<BaseItemRecipe>() 
+        { 
+            new BaseItemRecipe 
+            {
+                Origin = ItemType.Flashlight,
+                Target = new List<Probability<ItemType>>
+                {
+                    new Probability<ItemType> { result = ItemType.GrenadeFlash, amount = 1, rate = 75 },
+                    new Probability<ItemType> { result = ItemType.SCP2176, amount = 1, rate = 10 },
+                    new Probability<ItemType> { result = ItemType.Jailbird, amount = 1, rate = 1 },
+                    new Probability<ItemType> { result = ItemType.SCP207, amount = 1, rate = 4 }
+                },
+                Mode = Scp914.Scp914KnobSetting.VeryFine,
+                DestoryByDefault = true
+            },
+            new BaseItemRecipe
+            {
+                Origin = ItemType.KeycardO5,
+                Target = new List<Probability<ItemType>>
+                {
+                    new Probability<ItemType> { result = ItemType.KeycardFacilityManager, amount = 1, rate = 75 },
+                    new Probability<ItemType> { result = ItemType.MicroHID, amount = 1, rate = 5 }
+                },
+                Mode = Scp914.Scp914KnobSetting.Rough,
+                DestoryByDefault = false
+            }
+        };
+    }
+    public class PluginMain : Plugin<Config>
+    {
+        public override string Name => "Scp914Management";
+
+        public override string Description => "A plugin that can add new crafting recipes to 914";
+
+        public override string Author => "Honkai Impact 4: Galaxy Journey";
+
+        public override Version Version => new Version("1.0.0.0");
+
+        public override Version RequiredApiVersion => new Version("0.0.0.0");
+
+        public override void Disable()
+        {
+            ;
+        }
+        public override void Enable()
+        {
+            Scp914Handler.OnEnabled();
+            Scp914Handler.RegisterRecipes(Config.ItemRecipes);
+            Logger.Info("Plugin Loaded");
+        }
+    }
+    public class Scp914Handler
+    {
+        public static List<BaseItemRecipe> itemRecipes = new List<BaseItemRecipe>();
+        public static void OnEnabled()
+        {
+            Scp914Events.ProcessingInventoryItem += OnInventoryItemCrafting;
+            Scp914Events.ProcessingPickup += OnItemCrafting;
+        }
+        public static void RegisterRecipes(params BaseItemRecipe[] recipes)
+        {
+            foreach (var recipe in recipes)
+            {
+                itemRecipes.Add(recipe);
+            }
+        }
+        public static void RegisterRecipes(IEnumerable<BaseItemRecipe> recipes)
+        {
+            foreach (var recipe in recipes)
+            {
+                itemRecipes.Add(recipe);
+            }
+        }
+        public static void OnItemCrafting(Scp914ProcessingPickupEventArgs ev)
+        {
+            BaseItemRecipe recipe = (from item in itemRecipes
+                                    where item.Match(ev.Pickup.Type) && item.Mode == ev.KnobSetting
+                                    select item).FirstOrDefault();
+            recipe?.ProcessPickupCrafting(ev);
+        }
+        public static void OnInventoryItemCrafting(Scp914ProcessingInventoryItemEventArgs ev)
+        {
+            BaseItemRecipe recipe = (from item in itemRecipes
+                                     where item.Match(ev.Item.Type) && item.Mode == ev.KnobSetting
+                                     select item).FirstOrDefault();
+            recipe?.ProcessInventoryCrafting(ev);
+        }
     }
 }
--- Scp914Management/RecipeStructs.cs
+++ Scp914Management/RecipeStructs.cs
@@ -4,22 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LabApi.Events.Arguments.Scp914Events;
+using LabApi.Features.Console;
 using LabApi.Features.Wrappers;
+using Scp914;
 
 namespace Scp914Management
 {
-    public struct Probability<T>
+    public class Probability<T>
     {
-        public T item;
-        public int rate, num;
+        public T result { get; set; }
+        public int rate { get; set; }
+        public int amount { get; set; }
     }
     public class BaseItemRecipe
     {
         public ItemType Origin { get; set; }
         public List<Probability<ItemType>> Target { get; set; }
+        public Scp914KnobSetting Mode { get; set; }
         public bool DestoryByDefault { get; set; } = true;
         public virtual bool Match(ItemType origin) => Origin == origin;
-        public virtual void ProcessCrafting(Scp914ProcessingPickupEventArgs ev)
+        public virtual void ProcessPickupCrafting(Scp914ProcessingPickupEventArgs ev)
         {
             int sumrate = 0;
             foreach (var item in Target) 
@@ -29,14 +33,15 @@ namespace Scp914Management
             int num;
             if (sumrate > 100) num = new Random().Next(1, sumrate + 1);
             else num = new Random().Next(1, 101);
+            Logger.Info(num);
             foreach (var item in Target)
             {
                 if (num - item.rate <= 0)
                 {
                     ev.IsAllowed = false;
-                    for (int i = 0; i < item.num; ++i)
+                    for (int i = 0; i < item.amount; ++i)
                     {
-                        Pickup.Create(item.item, ev.NewPosition);
+                        Pickup.Create(item.result, ev.NewPosition).Spawn();
                     }
                     ev.Pickup.Destroy();
                     return;
@@ -49,6 +54,35 @@ namespace Scp914Management
                 ev.IsAllowed = false;
                 ev.Pickup.Position = ev.NewPosition;
             }
+        }
+        public virtual void ProcessInventoryCrafting(Scp914ProcessingInventoryItemEventArgs ev)
+        {
+            int sumrate = 0;
+            foreach (var item in Target)
+            {
+                sumrate += item.rate;
+            }
+            int num;
+            if (sumrate > 100) num = new Random().Next(1, sumrate + 1);
+            else num = new Random().Next(1, 101);
+            Logger.Info(num);
+            foreach (var item in Target)
+            {
+                if (num - item.rate <= 0)
+                {
+                    ev.IsAllowed = false;
+                    ev.Player.RemoveItem(ev.Item);
+                    for (int i = 0; i < item.amount; ++i)
+                    {
+                        if (!ev.Player.IsInventoryFull) ev.Player.AddItem(item.result);
+                        else Pickup.Create(item.result, Scp914Controller.Singleton.OutputChamber.position).Spawn();
+                    }
+                    return;
+                }
+                else num -= item.rate;
+            }
+            ev.IsAllowed = false;
+            if (DestoryByDefault) ev.Player.RemoveItem(ev.Item);
         }
     }
 }
